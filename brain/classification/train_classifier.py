ed1a6a1a79f05b8ac2b9cc77e40809bd5a2551f1
Killerkoen <koen.kraaijveld@gmail.com>
2023-01-28 03:45:36 +0100

    Added training script for classifier

---
+++ brain/classification/train_classifier.py
@@ -0,0 +1,100 @@
+from typing import List
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+from train_test_data import split_train_val_set
+
+
+class Classifier1(nn.Module):
+    def __init__(self):
+        super(Classifier1, self).__init__()
+        self.conv1 = nn.Conv2d(2, 64, kernel_size=(3, 3), padding=1)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1)
+        self.conv5 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1)
+        self.conv6 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1)
+        self.conv7 = nn.Conv2d(64, 3, kernel_size=(1, 1))
+        self.fc1 = nn.Linear(2688, 64)
+        self.value = nn.Linear(64, 1)
+        self.policy = nn.Linear(64, 7)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        return torch.tanh(self.value(x))
+        # return F.tanh(self.value(x)), F.softmax(self.policy(x), dim=-1)
+        # return F.log_softmax(self.policy(x), dim=-1)
+
+
+train_data, val_data = split_train_val_set(1)
+train_loader = DataLoader(train_data, batch_size=256, shuffle=True)
+val_loader = DataLoader(val_data, batch_size=256, shuffle=True)
+PATH = './connect_4.pth'
+
+
+def train(num_epochs):
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    criterion1 = nn.MSELoss()
+
+    model = Classifier1().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.00001)
+
+    loss_interval = 500
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            board_state, result = data
+            board_state = board_state.to(device)
+            result = result.to(device)
+
+            optimizer.zero_grad()
+
+            output_result = model(board_state).to(device)
+            loss = criterion1(output_result, result)
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % loss_interval == loss_interval - 1:
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / loss_interval:.3f}')
+                running_loss = 0.0
+
+    print('Finished Training')
+    torch.save(model.state_dict(), PATH)
+
+
+def test():
+    correct = 0
+    total = 0
+    model = Classifier1()
+    model.load_state_dict(torch.load(PATH))
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        for data in val_loader:
+            images, labels = data
+            # calculate outputs by running images through the network
+            outputs = model(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Accuracy: {100 * correct // total}%')
+
+
+train(num_epochs=10)
+# test()
--- brain/classification/train_test_data.py
+++ brain/classification/train_test_data.py
@@ -1,12 +1,33 @@
+import json
+from typing import List
+
+import torch
 import pandas as pd
+import numpy as np
+from torch.utils.data import DataLoader, Dataset, random_split
 
 
-def load_data(file_path: str):
+class Connect4Dataset(Dataset):
+    def __init__(self, csv_file, transform=None, label_transform=None):
+        data = pd.read_csv(csv_file)
+        self.data = data[["board_state", "optimal_move", "result"]].copy()
+        self.transform = transform
+        self.label_transform = label_transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        channel_p1, channel_p2 = split_board_state(self.data["board_state"][item])
+        board_state = torch.tensor(np.array([channel_p1, channel_p2], dtype=np.float32))
+        result = torch.tensor([self.data["result"][item]], dtype=torch.float32)
+        optimal_move = torch.tensor(self.data["optimal_move"][item])
+        return board_state, result
+
+
+def process_data(file_path: str):
     raw_data = pd.read_csv(file_path).drop_duplicates()
     raw_data = raw_data.sample(frac=1).reset_index(drop=True)
-    # print(raw_data)
-    # print(len(raw_data))
-    # print(raw_data["turn_num"].value_counts())
 
     filtered_turn_num = pd.DataFrame()
     for i in range(1, 22):
@@ -23,22 +44,20 @@ def load_data(file_path: str):
     processed_data_p1 = filtered_result.loc[filtered_result["player_turn"] == 1]
     processed_data_p2 = filtered_result.loc[filtered_result["player_turn"] == 2]
 
-    # print(processed_data_p1["player_turn"].value_counts())
-    # print(processed_data_p1["turn_num"].value_counts())
-    # print(processed_data_p1["optimal_move"].value_counts())
-    # print(processed_data_p1["result"].value_counts())
-
-    # print(processed_data_p2["player_turn"].value_counts())
-    # print(processed_data_p2["turn_num"].value_counts())
-    # print(processed_data_p2["optimal_move"].value_counts())
-    # print(processed_data_p2["result"].value_counts())
-
-    # print(filtered_result)
-
-    # filtered_result.to_csv("../../data/classification/processed_game_data.csv")
     processed_data_p1.to_csv("../../data/classification/processed_p1_game_data.csv")
     processed_data_p2.to_csv("../../data/classification/processed_p2_game_data.csv")
 
 
+def split_train_val_set(player_id: int):
+    dataset = Connect4Dataset(csv_file=f"../../data/classification/processed_p{player_id}_game_data.csv")
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_data, val_data = torch.utils.data.random_split(dataset, [train_size, val_size])
+    return train_data, val_data
 
-load_data("../../data/classification/game_data.csv")
+
+def split_board_state(board_str: str):
+    board_state = np.array(json.loads(board_str), dtype=np.float32).flatten()
+    channel_p1 = np.where(board_state == 2, 0, board_state).reshape((6, 7))
+    channel_p2 = np.where(board_state == 1, 0, board_state).reshape((6, 7))
+    return channel_p1, channel_p2
