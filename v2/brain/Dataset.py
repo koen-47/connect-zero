6ac1aff353b6cb10c53b18532c9610c249b83fc8
Killerkoen <koen.kraaijveld@gmail.com>
2023-10-21 01:48:17 +0200

    Fixed issue with reward allocation

--- v2/brain/Dataset.py
+++ v2/brain/Dataset.py
@@ -13,7 +13,9 @@ class Dataset:
             self.data = np.append(self.data, [[np.fliplr(state), np.flip(probs), player, None]], axis=0)
 
     def set_rewards(self, reward, player):
-        self.data[:, 3] = [-reward if player == self.data[i, 2] else reward for i in range(len(self.data))]
+        self.data[:, 3] = [reward if player == self.data[i, 2] else -reward for i in range(len(self.data))]
+        # temp = [reward * ((-1) ** (self.data[i, 2] != player)) for i in range(len(self.data))]
+        # print(np.array_equal(np.array(self.data[:, 3]), np.array(temp)))
 
     def shuffle(self):
         np.random.shuffle(self.data)
